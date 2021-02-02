1353b7ff02738a8de962e6e56141205a51e06c44
jemuel.castro <jemuel.castro@metrobank.com.ph>
2021-02-02 17:23:44 +0800

    initialize unit tests

--- src/test/java/com/jmc/productmanagementservice/ProductManagementServiceApplicationTests.java
+++ src/test/java/com/jmc/productmanagementservice/ProductManagementServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jmc.productmanagementservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ProductManagementServiceApplication.class)
 @SpringBootTest
 class ProductManagementServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testProductManagementServiceApplication() {
+		ProductManagementServiceApplication.main(new String[] {});
+		assertThat(1).isEqualTo(1);
 	}
 
 }
